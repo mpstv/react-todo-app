ebb0196a8b6fdf211be1262c34282d770110796f
mpstv <54762420+mpstv@users.noreply.github.com>
2021-02-05 14:58:45 +0300

    add prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('element was created', () => {
+test("element was created", () => {
   const renderResult = render(<App />);
   expect(renderResult.baseElement).toBeTruthy();
 });
